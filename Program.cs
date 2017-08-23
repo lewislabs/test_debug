a6af18b5761272694db484ab1b9d059b775963b4
Chris Lewis <chris.lewis@hudl.com>
2017-08-23 10:40:17 +0100

    Adding all the stuff

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+
+namespace test_debug
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            while (true)
+            {
+                Console.Out.WriteLine("Test");
+                Thread.Sleep(1000);
+
+            }
+        }
+    }
+}
